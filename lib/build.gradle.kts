862d0b138477724c0aa69d45dd36b53125c9210e
Brad Morgan <bmorgan1@urbn.com>
2025-10-06 15:11:34 -0400

    Setup maven publishing

--- lib/build.gradle.kts
+++ lib/build.gradle.kts
@@ -2,10 +2,15 @@ import org.gradle.api.tasks.testing.logging.TestExceptionFormat
 import org.gradle.api.tasks.testing.logging.TestLogEvent
 
 plugins {
-    id("org.jetbrains.kotlin.jvm") version "1.7.10"
+    id("org.jetbrains.kotlin.jvm") version "1.8.21"
     id("me.champeau.jmh") version "0.6.8"
+    id("maven-publish")
+    id("signing")
     `java-library`
 }
+
+group = "com.urbn.nu"
+version = "1.0.0-SNAPSHOT"
 
 repositories {
     mavenCentral()
@@ -19,7 +24,10 @@ dependencies {
 java {
     sourceCompatibility = JavaVersion.VERSION_17
     targetCompatibility = JavaVersion.VERSION_17
+    withJavadocJar()
+    withSourcesJar()
 }
+
 tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
     kotlinOptions {
         apiVersion = "1.8"
@@ -45,4 +53,61 @@ jmh {
     warmupIterations.set(2)
     iterations.set(2)
     fork.set(2)
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("mavenJava") {
+            artifactId = "sqlkraft"
+            from(components["java"])
+            versionMapping {
+                usage("java-api") {
+                    fromResolutionOf("runtimeClasspath")
+                }
+                usage("java-runtime") {
+                    fromResolutionResult()
+                }
+            }
+            pom {
+                name.set("SqlKraft")
+                description.set("Composable, parameterized SQL statements")
+                url.set("https://github.com/urbn/sqlkraft")
+                licenses {
+                    license {
+                        name.set("MIT License")
+                        url.set("https://opensource.org/license/mit/")
+                    }
+                }
+                developers {
+                    developer {
+                        name.set("Brad Morgan")
+                        email.set("bmorgan1@urbn.com")
+                        organization.set("URBN")
+                        organizationUrl.set("https://www.urbn.com")
+                    }
+                }
+                scm {
+                    connection.set("scm:git@github.com:urbn/sqlkraft.git")
+                    developerConnection.set("scm:git@github.com:urbn/sqlkraft.git")
+                    url.set("https://github.com/urbn/sqlkraft")
+                }
+            }
+        }
+    }
+
+    repositories {
+        maven {
+            name = "mavenCentral"
+            credentials(PasswordCredentials::class)
+            // change URLs to point to your repos, e.g. http://my.org/repo
+            val releasesRepoUrl = uri(layout.buildDirectory.dir("repos/releases"))
+//            val snapshotsRepoUrl = uri(layout.buildDirectory.dir("repos/snapshots"))
+            val snapshotsRepoUrl = uri("https://central.sonatype.com/repository/maven-snapshots/")
+            url = if (version.toString().endsWith("SNAPSHOT")) snapshotsRepoUrl else releasesRepoUrl
+        }
+    }
+}
+
+signing {
+    sign(publishing.publications["mavenJava"])
 }
